cd15e07b6059a16cc2239a189826b741401cdcc3
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-05-29 10:30:57 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <p>the new catalog is about to be here</p>;
+}
+
+export default App;

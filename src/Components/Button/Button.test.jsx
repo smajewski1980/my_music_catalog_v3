3731f2d3f2f785b6bdab98bd1e5c7f38ce445a94
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-06-08 14:47:09 -0400

    updated the tests for the Button component since we nixed using React Router

--- src/Components/Button/Button.test.jsx
+++ src/Components/Button/Button.test.jsx
@@ -1,32 +1,38 @@
 import "@testing-library/jest-dom/vitest";
-import { describe, it, expect, afterEach } from "vitest";
-import { render, screen } from "../test-utils";
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "../test-utils";
 import Button from "./Button";
+import React from "react";
 
 describe("Button", () => {
   it("renders a button with the text given through its props", () => {
     const testText = "test text";
-    render(
-      <Button
-        path='/'
-        text={testText}
-      />,
-    );
+    render(<Button text={testText} />);
 
     expect(screen.getByText(testText)).toBeInTheDocument();
   });
 
-  it("the Link has a to property value of the given prop", () => {
+  it("calls the given prop function with the given prop type", () => {
     const testText = "test text";
+    const newVal = "new value";
+    const stateSetter = vi.fn();
 
     render(
       <Button
-        path={"/test-page"}
+        func={stateSetter}
+        type={newVal}
         text={testText}
       />,
     );
+    const button = screen.getByText(testText);
 
-    const linkElement = screen.getByRole("link", { name: testText });
-    expect(linkElement).toHaveAttribute("href", "/test-page");
+    vi.spyOn(React, "useState").mockImplementation((initial) => [
+      initial,
+      stateSetter,
+    ]);
+
+    fireEvent.click(button);
+
+    expect(stateSetter).toHaveBeenCalledWith(newVal);
   });
 });
---
+++ src/Components/Search/Search.test.jsx
@@ -0,0 +1,8 @@
+import "@testing-library/jest-dom/vitest";
+import { describe, it, expect, afterEach } from "vitest";
+import { render, screen } from "../test-utils";
+import Search from "./Search";
+
+describe("Search", () => {
+  it.todo("test the correct dropdown options render");
+});

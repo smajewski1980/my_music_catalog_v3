1d4f66a93309446756c09ae816aa9f35ba61fea4
Steve <138328427+smajewski1980@users.noreply.github.com>
2026-09-25 13:17:24 -0400

    added the logic to have the total item qty be dynamic

--- src/Components/TotalItemQty/TotalItemQty.jsx
+++ src/Components/TotalItemQty/TotalItemQty.jsx
@@ -1,10 +1,28 @@
 import styles from "./TotalItemQty.module.css";
+import { CatalogContext } from "../../Context/CatalogContext";
+import { useEffect, useContext, useState } from "react";
 
 const TotalItemQty = () => {
+  const [totalItemQty, setTotalItemQty] = useState(0);
+  const { recordsData, tapesData, cdsData, cdCompsData, cdSinglesData } =
+    useContext(CatalogContext);
+
+  const recordsQty = recordsData.length;
+  const tapesQty = tapesData.length;
+  const cdsQty = cdsData.length;
+  const cdCompsQty = cdCompsData.length;
+  const cdSinglesQty = cdSinglesData.length;
+
+  useEffect(() => {
+    setTotalItemQty(
+      () => recordsQty + tapesQty + cdsQty + cdCompsQty + cdSinglesQty,
+    );
+  }, []);
+
   return (
     <div className={styles.totalQtyWrapper}>
       <p>
-        Home to over <span id=''>XX,XXX</span>&nbsp;items.
+        Home to over <span>{totalItemQty}</span>&nbsp;items.
       </p>
     </div>
   );
